c551c5698d561d22b32bba926c8d02b222f4523d
oosmacher@gmail.com <oosmacher@gmail.com>
2025-01-31 14:35:12 +0200

    Fix App component types

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import { ThemeProvider } from "@/components/theme-provider"
+import type { AppProps } from 'next/app'
 
-export default function App({ Component, pageProps }) {
+export default function App({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider
       attribute="class"
