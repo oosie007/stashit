dab4c1becca2c48f4563f33e3d3acbadca0b6ef4
oosmacher@gmail.com <oosmacher@gmail.com>
2025-02-10 10:31:24 +0200

    feat: added background scraping 7

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,13 +1,13 @@
 import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
-import sanitizeHtmlLib from 'sanitize-html'
+import sanitizeHtml from 'sanitize-html'
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
 
 export const sanitizeHtml = (html: string) => {
-  return sanitizeHtmlLib(html, {
+  return sanitizeHtml(html, {
     allowedTags: [
       'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'p', 'a', 'ul', 'ol', 
       'li', 'b', 'i', 'strong', 'em', 'blockquote', 'br', 'div',
