1b2b921b6534a2e167cc606604a7c3e03e0c95e9
oosmacher@gmail.com <oosmacher@gmail.com>
2025-02-10 08:51:04 +0200

    feat: fix vercel

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -71,13 +71,13 @@ type LayoutType = 'card' | 'list'
 
 type CategoryType = 'all' | 'articles' | 'highlights' | 'loved'
 
-const QuoteIcon = () => (
+const QuoteIcon = ({ className = "h-4 w-4 mb-2" }: { className?: string }) => (
   <svg 
     xmlns="http://www.w3.org/2000/svg" 
     width="24" 
     height="24" 
     viewBox="0 0 24 24" 
-    className="h-4 w-4 mb-2"
+    className={className}
     fill="currentColor"
   >
     <path d="M16 3a2 2 0 0 0-2 2v6a2 2 0 0 0 2 2 1 1 0 0 1 1 1v1a2 2 0 0 1-2 2 1 1 0 0 0-1 1v2a1 1 0 0 0 1 1 6 6 0 0 0 6-6V5a2 2 0 0 0-2-2z" />
